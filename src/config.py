231065eeafdb2ded46a54b92270f9d8d878d72af
Jad256 <100147337+Jad256@users.noreply.github.com>
2025-05-18 23:19:50 +0300

    Delete src directory

--- src/config.py
+++
@@ -1,4 +0,0 @@
-# src/config.py
-DATA_PATH = 'data/creditcard.csv'
-MODEL_PATH = 'models\Trained_model.pkl'
-SCALER_PATH = 'data\scaler.pkl'
--- src/utilities.py
+++
@@ -1,8 +0,0 @@
-import logging
-
-def setup_logging():
-    logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
-
-def load_data(path):
-    import pandas as pd
-    return pd.read_csv(path)
